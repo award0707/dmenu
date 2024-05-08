c385a9a7b4bf0fa0c103ca955c645e10bebaa04c
Andrew Ward <wardac@vcu.edu>
2024-05-08 11:52:33 -0400

    working

--- config.def.h
+++ config.def.h
@@ -6,7 +6,8 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
+	"Font Awesome:size=10",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static char *colors[SchemeLast][2] = {
@@ -25,4 +26,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 3;
+static unsigned int border_width = 2;
