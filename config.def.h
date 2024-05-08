b23458246a5447d4d91a58d85cc0cde16aa8a6bd
Andrew Ward <wardac@vcu.edu>
2024-05-08 11:57:29 -0400

    date patch

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char *fonts[] = {
-	"monospace:size=10",
-	"Font Awesome:size=10",
+	"Liberation Mono:size=8",
+	"SymbolsNerdFont:size=10",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static char *colors[SchemeLast][2] = {
